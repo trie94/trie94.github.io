45dc2202ff8b9e4e213800713c1b1419d4d84c5e
trie94 <trie60@gmail.com>
2018-08-16 11:54:00 -0400

    basic setup prod/dev

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import '../css/style.css';
+
+if (process.env.NODE_ENV !== 'production') { console.log("dev mode"); }
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => {
+        const oldRootElem = document.querySelector('body');
+        const newRootElem = oldRootElem.cloneNode(false);
+        oldRootElem.parentNode.insertBefore(newRootElem, oldRootElem);
+        oldRootElem.parentNode.removeChild(oldRootElem);
+    });
+} else {
+    enableProdMode();
+}
---
+++ webpack.common.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: ['./src/js/index.js'],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        chunkFilename: 'chunks/[id].[chunkhash].js'
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        },
+        {
+            test: /\.html$/,
+            use: {
+                loader: 'html-loader',
+                options: { minimize: true }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'css-hot-loader',
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ],
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name:'[path][name].[ext]'
+                    }
+                }
+            ]
+        }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: './src/html/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            title: 'Production',
+            chunkFilename: '[id].css'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
