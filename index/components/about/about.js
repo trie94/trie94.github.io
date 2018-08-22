89fd689b3e7d0b50651bc3df4e537ee7a7387efb
trie94 <trie60@gmail.com>
2018-08-22 14:42:31 -0400

    add texture, change imges, and add a light switch

--- index/components/about/about.js
+++ index/components/about/about.js
@@ -2,18 +2,34 @@ import React from 'react';
 import './about.scss';
 import about_content from './about.json';
 
+import door from '../../../assets/imgs/door.png';
+import light from '../../../assets/imgs/light-switch.png';
+
 class About extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isLightOn: true,
+            backgroundColor: "rgb(1,1,1,0.0)" };
+
         this.content = about_content.intro;
+
+        this.style = { backgroundColor: this.state.backgroundColor }
+        this.lightHandler = this.lightHandler.bind(this);
+    }
+
+    lightHandler(){
+        this.setState({ isLightOn: !this.state.isLightOn }, ()=>
+            this.setState({ backgroundColor: this.state.isLightOn ? "#fff" : "rgb(0,0,0,0.9)" }));
     }
 
     render() {
+        this.style = { backgroundColor: this.state.backgroundColor };
         return (
-            <div className='about row'>
+            <div className='about row' style={this.style}>
                 <div className='interaction'>
-                    img-test: router
+                    <img className="switch-img" src={light} onClick={this.lightHandler}/>
+                    <img className="door-img" src={door}/>
                 </div>
                 <div className='description'>
                     {this.content}
