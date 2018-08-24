0cf528b3114505f6f1c2ae768a9796821d806eda
trie94 <trie60@gmail.com>
2018-08-24 14:37:14 -0400

    add thumbnail lines, need to get fixed

--- index/components/work/work.js
+++ index/components/work/work.js
@@ -8,6 +8,7 @@ import arrow from '../../../assets/imgs/arrow.png';
 import brick_thumb from '../../../assets/imgs/thumbnails/brick_thumb.png';
 import tilt_thumb from '../../../assets/imgs/thumbnails/tilt_thumb.png';
 import frankenstein_thumb from '../../../assets/imgs/thumbnails/frankenstein_thumb.png';
+import thumb_frame from '../../../assets/imgs/thumbnails/thumb_frame.png';
 
 class Work extends React.Component {
     constructor(props) {
@@ -21,16 +22,19 @@ class Work extends React.Component {
                 <div id="work">WORK</div>
                 <div className="row">
                     <div className='dev1'>
-                        <Link to="/brick" ><img className="thumb-img" src={brick_thumb}/></Link>
+                        <img className="thumb-frame" src={thumb_frame} />
+                        <Link to="/brick" ><img className="thumb-img" src={brick_thumb} /></Link>
                     </div>
                     <div className='dev2'>
-                        <Link to="/tilt"><img className="thumb-img" src={tilt_thumb}/></Link>
+                        <img className="thumb-frame" src={thumb_frame} />
+                        <Link to="/tilt"><img className="thumb-img" src={tilt_thumb} /></Link>
                     </div>
                     <div className='dev3'>
-                        <Link to="/frankenstein"><img className="thumb-img" src={frankenstein_thumb}/></Link>
+                        <img className="thumb-frame" src={thumb_frame} />
+                        <Link to="/frankenstein"><img className="thumb-img" src={frankenstein_thumb} /></Link>
                     </div>
                     <div className="readmore"><p className="see-more"><Link to="/works">SEE MORE</Link></p>
-                    <Link to="/works"><img className="arrow" src={arrow} ></img></Link>
+                        <Link to="/works"><img className="arrow" src={arrow} ></img></Link>
                     </div>
                     <img className="row-line" src={border} />
                 </div>
