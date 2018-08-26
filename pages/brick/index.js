266b3a1cd859785fb8846b73cf49cd0386749a21
trie94 <trie60@gmail.com>
2018-08-25 22:59:30 -0400

    brick init post layout

--- pages/brick/index.js
+++ pages/brick/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './brick.scss';
 import Header from '../../common/header/header';
+import content from './brick.json';
 
 class Brick extends React.Component {
     constructor(props) {
@@ -13,8 +14,11 @@ class Brick extends React.Component {
         return (
             <div>
                 <Header title="BRICK" link="/brick" />
-                <div className='brick'>
-                brick
+                <div className='work-content'>
+                    <div className="work-title">{content.title}</div>
+                    <div className="video-wrapper">
+                    <iframe className="work-video" src="https://www.youtube.com/embed/QHMHPzehOLA" /></div>
+                    <div className="work-summary">{content.summary}</div>
                 </div>
             </div>
         )
