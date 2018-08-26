1f10a4f013d8f06ed1c030409572abb7b5076588
trie94 <trie60@gmail.com>
2018-08-26 19:51:43 -0400

    done with brick page

--- pages/brick/index.js
+++ pages/brick/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../pages.scss';
+import './brick.scss';
+
 import Header from '../../common/header/header';
 import Summary from '../summary-template';
 import content from './brick.json';
+
+import asym1 from '../../assets/imgs/brick/asymmetric1.jpg';
+import asym2 from '../../assets/imgs/brick/asymmetric2.jpg';
+import spawn_wall from '../../assets/imgs/brick/spawn_wall.gif';
+import grab1 from '../../assets/imgs/brick/grab1.gif';
+import grab2 from '../../assets/imgs/brick/grab2.gif';
 
 class Brick extends React.Component {
     constructor(props) {
@@ -15,7 +23,31 @@ class Brick extends React.Component {
         return (
             <div>
                 <Header title={content.title} link={content.link} />
-                <Summary content={content}/>
+                <Summary content={content} />
+                <div className="main-content">
+                    <p className="sub-titles">grab, match, and collaborate!</p>
+                    <p className="section-wrappers">{content.section1}</p>
+
+                    <div className="sections">
+                        <p className="sub-titles">1. get colors (asymmetric role)</p>
+                        <div className="content-img-wrapper">
+                            <img className="content-img content-img-left" src={asym1} />
+                            <img className="content-img content-img-right" src={asym2} />
+                        </div>
+                        <p className="section-wrappers">{content.section2}</p>
+                        <p className="sub-titles">2. spawn wall</p>
+                        <div className="content-img-wrapper">
+                            <img className="content-img" src={spawn_wall} />
+                        </div>
+                        <p className="section-wrappers">{content.section3}</p>
+                        <p className="sub-titles">3. grab a block and match it to grid</p>
+                        <div className="content-img-wrapper">
+                            <img className="content-img content-img-left" src={grab1} />
+                            <img className="content-img content-img-right" src={grab2} />
+                        </div>
+                        <p className="section-wrappers">{content.section4}</p>
+                    </div>
+                </div>
             </div>
         )
     }
