a6587bef984755835f68959f7a3dcea9bc1f8728
trie94 <trie60@gmail.com>
2018-08-28 00:14:03 -0400

    add hover summary

--- index/components/work/work.js
+++ index/components/work/work.js
@@ -10,6 +10,10 @@ import tilt_thumb from '../../../assets/imgs/thumbnails/tilt_thumb.png';
 import frankenstein_thumb from '../../../assets/imgs/thumbnails/frankenstein_thumb.png';
 import thumb_frame from '../../../assets/imgs/thumbnails/thumb_frame.png';
 
+import brick_content from '../../../pages/brick/brick.json';
+import tilt_content from '../../../pages/tilt/tilt.json';
+import frankenstein_content from '../../../pages/frankenstein/frankenstein.json';
+
 class Work extends React.Component {
     constructor(props) {
         super(props);
@@ -23,17 +27,29 @@ class Work extends React.Component {
                 <div className="row">
                     <div className='dev1'>
                         <Link to="/brick" ><img className="thumb-frame" src={thumb_frame} />
-                            <img className="thumb-img" src={brick_thumb} /><div className="overlay">this is title</div></Link>
+                            <img className="thumb-img" src={brick_thumb} />
+                            <div className="overlay">
+                            <div className="overlay-content"><strong>{brick_content.title}</strong>
+                            <br/><br/>{brick_content.summary}
+                            </div></div></Link>
                     </div>
 
                     <div className='dev2'>
                         <Link to="/tilt"><img className="thumb-frame" src={thumb_frame} />
-                            <img className="thumb-img" src={tilt_thumb} /><div className="overlay">this is title</div></Link>
+                            <img className="thumb-img" src={tilt_thumb} />
+                            <div className="overlay">
+                            <div className="overlay-content"><strong>{tilt_content.title}</strong>
+                            <br/><br/>{tilt_content.summary}
+                            </div></div></Link>
                     </div>
 
                     <div className='dev3'>
                         <Link to="/frankenstein"><img className="thumb-frame" src={thumb_frame} />
-                            <img className="thumb-img" src={frankenstein_thumb} /><div className="overlay">this is title</div></Link>
+                            <img className="thumb-img" src={frankenstein_thumb} />
+                            <div className="overlay">
+                            <div className="overlay-content"><strong>{frankenstein_content.title}</strong>
+                            <br/><br/>{frankenstein_content.summary}
+                            </div></div></Link>
                     </div>
 
                     <div className="readmore"><p className="see-more"><Link to="/works">SEE MORE</Link></p>
