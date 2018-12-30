01fe68434d24867f558238b330f088fbcbff4486
trie94 <trie60@gmail.com>
2018-12-30 18:19:51 +0900

    fix positioning in the blog section

--- index/components/blog/blog.js
+++ index/components/blog/blog.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './blog.scss';
-import border from '../../../assets/imgs/border.png';
-import { Link } from 'react-router-dom';
 import sketchbook from '../../../assets/imgs/thumbnails/sketchbook.png';
 import sketchbook_hover from '../../../assets/imgs/thumbnails/sketchbook_hover.png';
 import sandbox from '../../../assets/imgs/thumbnails/sandbox.png';
@@ -18,8 +16,8 @@ class Blog extends React.Component {
     render() {
         return (
             <div className='blog'>
-                <p id="small-stuff" className="titles"> SMALL STUFF</p>
-                <div className='row blog'>
+                <div id="small-stuff" className="titles"> SMALL STUFF</div>
+                <div className='row'>
                     <div className='blog1'>
                         <Thumb img={sketchbook} hoverimg={sketchbook_hover}
                         link ="https://www.instagram.com/crushed157/" title="DRAWINGS"/>
@@ -64,8 +62,8 @@ class Thumb extends React.Component{
 
         return (
             <a href={this.props.link} target="_blank" style={this.style}>
-            <div className="stuff-thumbnails"><img src ={this.img}
-            onMouseEnter={this.hover} onMouseLeave={this.notHover} /></div>
+            <img className="thumb-img" src ={this.img}
+            onMouseEnter={this.hover} onMouseLeave={this.notHover} />
             <p className="thumb-text">{this.props.title}</p></a>
         );
     }
