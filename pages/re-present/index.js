5c88d7b0e23855cca096b4569fc996f3e0cd622a
trie94 <trie60@gmail.com>
2019-01-01 23:24:47 +0900

    add data part in the re-present

--- pages/re-present/index.js
+++ pages/re-present/index.js
@@ -6,7 +6,13 @@ import Header from '../../common/header/header';
 import Summary from '../summary-template';
 import content from './represent.json';
 
-import experience_flow from '../../assets/imgs/re-present/re-present-experience-flow.png';
+import experience_flow from '../../assets/imgs/re-present/re-present_experience_flow.png';
+import data_flow from '../../assets/imgs/re-present/re-present_data_flow.png';
+import trackers from '../../assets/imgs/re-present/re-present_trackers.png';
+import analyzers from '../../assets/imgs/re-present/re-present_analyzers.png';
+import summary from '../../assets/imgs/re-present/re-present_session_summary.png';
+import data_framework from '../../assets/imgs/re-present/re-present_data_framework.png';
+import data_doc from '../../assets/pdfs/re-present_data_documentation.pdf';
 
 class Represent extends React.Component {
     constructor(props) {
@@ -24,8 +30,24 @@ class Represent extends React.Component {
                     <img className="content-full-img" src={experience_flow} />
                     <p className="section-wrappers post">{content.section1}</p>
                     <p className="sub-titles">Data Flow</p>
-                    <p className="section-wrappers post">{content.section2}</p>
-                </div>
+                    <img className="content-full-img" src={data_flow} />
+                    <p className="section-wrappers-continue post">{content.section2}</p>
+                    <p className="section-wrappers-continue post">{content.section3}</p>
+                    <img className="content-full-img" src={trackers} />
+                    <p className="section-wrappers-continue post">{content.section4}</p>
+                    <img className="content-full-img" src={analyzers} />
+                    <p className="section-wrappers-continue post">{content.section5}</p>
+                    <img className="content-full-img" src={summary} />
+                    <p className="section-wrappers post">{content.section6}</p>
+                    <p className="sub-titles">Data Structure</p>
+                    <img className="content-full-img" src={data_framework} />
+                    <p className="section-wrappers post">{content.section7}
+                    <br />
+                    <a href={data_doc} target="_blank">More detailed data documentation can be found in here.</a></p>
+                    <p className="sub-titles">Timeline Interaction</p>
+                    <p className="section-wrappers-continue post">{content.section8}</p>
+                    <p className="section-wrappers post">{content.section9}</p>
+                    </div>
             </div>
         )
     }
