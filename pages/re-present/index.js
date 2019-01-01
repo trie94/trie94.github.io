2445f02227b664737020554dc7cadd1b86d0adab
trie94 <trie60@gmail.com>
2019-01-01 17:23:17 +0900

    working on the represent page

--- pages/re-present/index.js
+++ pages/re-present/index.js
@@ -6,6 +6,8 @@ import Header from '../../common/header/header';
 import Summary from '../summary-template';
 import content from './represent.json';
 
+import experience_flow from '../../assets/imgs/re-present/re-present-experience-flow.png';
+
 class Represent extends React.Component {
     constructor(props) {
         super(props);
@@ -18,6 +20,11 @@ class Represent extends React.Component {
                 <Header title={content.title} link={content.link} />
                 <Summary content={content} />
                 <div className="main-content">
+                    <p className="sub-titles">Practice-Review Iteration Cycle</p>
+                    <img className="content-full-img" src={experience_flow} />
+                    <p className="section-wrappers post">{content.section1}</p>
+                    <p className="sub-titles">Data Flow</p>
+                    <p className="section-wrappers post">{content.section2}</p>
                 </div>
             </div>
         )
