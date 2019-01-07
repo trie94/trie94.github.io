95bb4ec48713b3c9cb41cff8a6f5151bfca1d922
trie94 <trie60@gmail.com>
2019-01-07 15:48:35 +0900

    minor change

--- index/components/blog/blog.js
+++ index/components/blog/blog.js
@@ -20,15 +20,15 @@ class Blog extends React.Component {
                 <div className='row'>
                     <div className='blog1'>
                         <Thumb img={sketchbook} hoverimg={sketchbook_hover}
-                        link ="https://www.instagram.com/crushed157/" title="DRAWINGS"/>
+                            link="https://www.instagram.com/crushed157/" title="DRAWINGS" />
                     </div>
                     <div className='blog2'>
                         <Thumb img={sandbox} hoverimg={sandbox_hover}
-                        link="https://trie94.github.io/sketchbook" title="CODING SKETCHES"/>
+                            link="https://trie94.github.io/sketchbook" title="CODING SKETCHES" />
                     </div>
                     <div className='blog3'>
                         <Thumb img={pastwork} hoverimg={pastwork_hover}
-                        link="/past-work" title="PAST WORK"/>
+                            link="/past-work" title="PAST WORK" />
                     </div>
                 </div>
             </div>
@@ -36,35 +36,37 @@ class Blog extends React.Component {
     }
 }
 
-class Thumb extends React.Component{
-    constructor(props){
+class Thumb extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={ isHovering: false };
+        this.state = { isHovering: false };
         this.hover = this.hover.bind(this);
         this.notHover = this.notHover.bind(this);
         this.img = this.props.img;
+
+        this.isPastwork = (this.props.link.indexOf("past") !== -1) ? null : "_blank";
     }
 
-    hover(){
-        if(this.state.isHovering === false){
-            this.setState({isHovering: true})
+    hover() {
+        if (this.state.isHovering === false) {
+            this.setState({ isHovering: true })
         }
     }
 
-    notHover(){
-        if(this.state.isHovering === true){
-            this.setState({isHovering: false})
+    notHover() {
+        if (this.state.isHovering === true) {
+            this.setState({ isHovering: false })
         }
     }
 
     render() {
-        this.img = this.state.isHovering? this.props.hoverimg : this.props.img;
+        this.img = this.state.isHovering ? this.props.hoverimg : this.props.img;
 
         return (
-            <a href={this.props.link} target="_blank" style={this.style}>
-            <img className="thumb-img" src ={this.img}
-            onMouseEnter={this.hover} onMouseLeave={this.notHover} />
-            <p className="thumb-text">{this.props.title}</p></a>
+            <a href={this.props.link} target={this.isPastwork} style={this.style}>
+                <img className="thumb-img" src={this.img}
+                    onMouseEnter={this.hover} onMouseLeave={this.notHover} />
+                <p className="thumb-text">{this.props.title}</p></a>
         );
     }
 }
