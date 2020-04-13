7f41cb4be220cb0d66eaaa44ca683a3cca431015
trie94 <trie60@gmail.com>
2020-04-12 20:44:51 -0700

    revise about intro and remove unused style

--- index/components/about/about.js
+++ index/components/about/about.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import './about.scss';
 import about_content from './about.json';
-import landing from './../../../assets/imgs/about/landing.png';
 
 class About extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isLightOn: true,
-            backgroundColor: "transparent"
-        };
-
-        this.content = about_content.intro;
-
-        this.style = { backgroundColor: this.state.backgroundColor }
-        this.lightHandler = this.lightHandler.bind(this);
-    }
-
-    lightHandler() {
-        this.setState({ isLightOn: !this.state.isLightOn }, () =>
-            this.setState({ backgroundColor: this.state.isLightOn ? "transparent" : "rgb(0,0,0,0.9)" }));
+        this.content = about_content.intro.replace("\\n", "\n");
+        console.log(this.content);
     }
 
     render() {
-        this.style = { backgroundColor: this.state.backgroundColor };
         return (
             <div className='about row' style={this.style}>
-                <div className='landing'>
-                </div >
             <div className='description'>
                 {this.content}
             </div>
